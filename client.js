aefef9f480c593bac806f3e344db69c37a8f6003
Alexander <akrichevcov96@mail.ru>
2021-06-24 12:56:36 +0500

    Промежуточный коммит

--- client.js
+++ client.js
@@ -1,7 +1,19 @@
 const { axios } = require("./fakeBackend/mock");
 
 const getEvents = async (formatDates = false) => {
-  // TODO
+   // await axios.get('/events').then(response => console.log(response))
+   const result = await axios.get('/tracks').then(response => response.data.tracks);
+   // console.log(result);
+
+   for (track in result) {
+      result[track].forEach(e => {
+         console.log(e.id);
+      });
+      // result[track]
+      // axios.get('/events' + track).then(response => console.log(response));
+   }
 };
+
+getEvents();
 
 module.exports = { getEvents };
