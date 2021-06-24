eb6cc17ef4e6cfad8801e5fdd97b6f0af6f1f831
Alexander <akrichevcov96@mail.ru>
2021-06-24 14:42:10 +0500

    Завершено, проходит 4 теста из 5.

--- client.js
+++ client.js
@@ -1,19 +1,32 @@
-const { axios } = require("./fakeBackend/mock");
+const {axios} = require("./fakeBackend/mock");
 
 const getEvents = async (formatDates = false) => {
-   // await axios.get('/events').then(response => console.log(response))
-   const result = await axios.get('/tracks').then(response => response.data.tracks);
-   // console.log(result);
+    const response = await axios.get('/tracks').then(response => response.data.tracks);
+    const result = {"events": []}
 
-   for (track in result) {
-      result[track].forEach(e => {
-         console.log(e.id);
-      });
-      // result[track]
-      // axios.get('/events' + track).then(response => console.log(response));
-   }
+    for (track in response) {
+        const lastEvent = response[track].length - 1
+        const date = response[track][lastEvent].date;
+        const event = await requestEvent(response[track].pop().id).then(response => response["events"][0]["name"]);
+        let test = new Date(date);
+        test = `${test.getDate()}-${test.getMonth() + 1}-${test.getFullYear()} ${test.getHours()}:${test.getMinutes()}:${test.getSeconds()}`
+        result.events.push({
+            "trackId": track,
+            "event": event,
+            "date": formatDates ? test : date
+        });
+    }
+    return result;
 };
+
+const requestEvent = async (id) => {
+    try {
+        return await axios.get('/events', {params: {id: id}}).then(response => response.data);
+    } catch (e) {
+        console.log(e.response.data);
+    }
+}
 
 getEvents();
 
-module.exports = { getEvents };
+module.exports = {getEvents};
