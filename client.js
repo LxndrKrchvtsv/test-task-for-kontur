124aac3cb07ea54d726e63292b5fdac97bfccf8e
Alexander <akrichevcov96@mail.ru>
2021-06-24 14:44:54 +0500

    Fix date.

--- client.js
+++ client.js
@@ -8,12 +8,12 @@ const getEvents = async (formatDates = false) => {
         const lastEvent = response[track].length - 1
         const date = response[track][lastEvent].date;
         const event = await requestEvent(response[track].pop().id).then(response => response["events"][0]["name"]);
-        let test = new Date(date);
-        test = `${test.getDate()}-${test.getMonth() + 1}-${test.getFullYear()} ${test.getHours()}:${test.getMinutes()}:${test.getSeconds()}`
+        let getDateEvent = new Date(date);
+        getDateEvent = `${test.getDate()}-${test.getMonth() + 1}-${test.getFullYear()} ${test.getHours()}:${test.getMinutes()}:${test.getSeconds()}`
         result.events.push({
             "trackId": track,
             "event": event,
-            "date": formatDates ? test : date
+            "date": formatDates ? getDateEvent : date
         });
     }
     return result;
@@ -26,7 +26,5 @@ const requestEvent = async (id) => {
         console.log(e.response.data);
     }
 }
-
-getEvents();
 
 module.exports = {getEvents};
